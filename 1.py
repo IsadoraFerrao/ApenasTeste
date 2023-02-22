59096b01b9c539f93a8de319ca3394dd12349e3b
WEVERTON MAGALHAES DA SILVA <wevertonmagsilva1@gmail.com>
2023-02-22 05:33:17 -0300

    exercicio 1
    foram feitas varias mudanças, foi criada uma lista.

--- 1.py
+++ 1.py
@@ -8,16 +8,26 @@ class Bola:
         self.cor = str(input('Qual a cor da bola? '))
         self.circunferencia = str(input('Qual a circunferência da bola? '))
         self.material = str(input('Qual o material da bola? '))
+        lista.append([self.cor,self.circunferencia,self.material])
+        # lista.append(self.circunferencia)
+        # lista.append(self.material)
 
     def ExibirInformacoesDestaBola(self):
        print(self.cor,self.circunferencia,self.material)  
+    
 
-
+lista = []
 
 while True:
-
     bola1 = Bola('','','')
-    bola1.ExibirInformacoesDestaBola()
     pergunta = str(input('Quer continuar? [S/N] '))
-    if pergunta in 'Nn':
-        break
+    if pergunta in 'Ss':
+        pass
+    else:
+        if pergunta in 'Nn':
+            break
+print('=-'*30)
+print(f'{"No.":<4}{"COR":<3}{"CIRCUNFERÊNCIA":>20}{"MATERIAL":>20}')
+print('=-'*30)
+for i, v in enumerate(lista):
+    print(f'{i:<4}{v[0]:<15}{v[1]:<20}{v[2]:<30}')
