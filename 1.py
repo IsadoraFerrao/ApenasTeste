6746fdfd2a915039009e86ec7ae173423e9b0ce8
Isadora Garcia Ferrão <isadora-gf@hotmail.com>
2023-02-22 09:51:05 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -13,21 +13,35 @@ class Bola:
         # lista.append(self.material)
 
     def ExibirInformacoesDestaBola(self):
-       print(self.cor,self.circunferencia,self.material)  
-    
+        print('=-'*30)
+        print(f'{"No.":<4}{"COR":<3}{"CIRCUNFERÊNCIA":>20}{"MATERIAL":>20}')
+        print(self.cor,self.circunferencia,self.material)  
+        print('=-'*30)
+        for i, v in enumerate(lista):
+            print(f'{i:<4}{v[0]:<15}{v[1]:<20}{v[2]:<30}')
+        
+    def MudarInformacoesBola(self):
+        self.cor = str(input('Qual a cor da bola? '))
+        self.circunferencia = str(input('Qual a circunferência da bola? '))
+        self.material = str(input('Qual o material da bola? '))
+        lista.append([self.cor,self.circunferencia,self.material])
 
 lista = []
+bola1 = Bola('','','')
 
 while True:
-    bola1 = Bola('','','')
     pergunta = str(input('Quer continuar? [S/N] '))
     if pergunta in 'Ss':
-        pass
+        opcao = input('Digite a opção desejada: \n1-Exibir informações\n2-Alterar bola\n->')
+        if opcao == '1':
+            bola1.ExibirInformacoesDestaBola()
+            # chamar aqui o metodo de exibir informacoes
+        else:
+            bola1.MudarInformacoesBola()
+            # chamar aqui o metodo de alteracao
     else:
         if pergunta in 'Nn':
             break
-print('=-'*30)
-print(f'{"No.":<4}{"COR":<3}{"CIRCUNFERÊNCIA":>20}{"MATERIAL":>20}')
-print('=-'*30)
-for i, v in enumerate(lista):
-    print(f'{i:<4}{v[0]:<15}{v[1]:<20}{v[2]:<30}')
+        
+#for i, v in enumerate(lista):
+    #print(f'{i:<4}{v[0]:<15}{v[1]:<20}{v[2]:<30}')
